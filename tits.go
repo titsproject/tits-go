0e0bf8c0224eb9537b560740b9e19431f4f9085d
Paul Rock <paul@drteam.rocks>
2017-08-03 19:28:34 +0300

    field of zeros

--- tits.go
+++ tits.go
@@ -1,1 +1,26 @@
-package tits_go
+package main
+
+import "fmt"
+
+func main() {
+
+	tits := [30][74]string{}
+	n := ""
+
+	for i := 0; i <= 29; i++ {
+		if i < 10 {
+			n = " "
+		} else {
+			n = ""
+		}
+
+		fmt.Print(n, i, " ")
+
+		for j := 0; j <= 73; j++ {
+			fmt.Print(tits[i][j], "0")
+		}
+
+		fmt.Println()
+	}
+
+}
